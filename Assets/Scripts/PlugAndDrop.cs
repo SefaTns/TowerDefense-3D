b8b645d75a38513f2d9a79fdd05de97a4ef19aaa
Sude Nihan <109664686+SudeNihanGurbuz@users.noreply.github.com>
2024-05-08 00:05:54 +0300

    level 2 sude

---
+++ Assets/Scripts/PlugAndDrop.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlugAndDrop: MonoBehaviour
+{
+    public GameObject objectPrefab; // Oluşturulacak nesnenin prefabı
+
+    void Update()
+    {
+        //// Fare tıklamasını algıla
+        //if (Input.GetMouseButtonDown(0)) // Sol tıklama
+        //{
+        //    // Fare tıklama konumunu al
+        //    Vector3 clickPosition = Input.mousePosition;
+        //    Debug.Log(Input.mousePosition);
+        //    Debug.Log(clickPosition);
+        //    //clickPosition.z = 10f;
+
+        //    // Yeni nesneyi oluştur
+        //    Instantiate(objectPrefab, clickPosition, Quaternion.identity);
+        //    Debug.Log(Quaternion.identity);
+        //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 clickPosition = hit.point;
+                Instantiate(objectPrefab, clickPosition, Quaternion.identity);
+            }
+        }
+    }
+}
