56bd15957690b473fa0725356d68bc830ac409bd
Sude Nihan <109664686+SudeNihanGurbuz@users.noreply.github.com>
2024-05-10 14:28:37 +0300

    tower in collider

--- Assets/Scripts/PlugAndDrop.cs
+++ Assets/Scripts/PlugAndDrop.cs
@@ -1,33 +1,22 @@
 ﻿using UnityEngine;
 
-public class PlugAndDrop: MonoBehaviour
+public class PlugAndDrop : MonoBehaviour
 {
-    public GameObject objectPrefab; // Oluşturulacak nesnenin prefabı
+    public GameObject objectPrefab; 
+    public Collider areaCollider; 
 
     void Update()
     {
-        //// Fare tıklamasını algıla
-        //if (Input.GetMouseButtonDown(0)) // Sol tıklama
-        //{
-        //    // Fare tıklama konumunu al
-        //    Vector3 clickPosition = Input.mousePosition;
-        //    Debug.Log(Input.mousePosition);
-        //    Debug.Log(clickPosition);
-        //    //clickPosition.z = 10f;
-
-        //    // Yeni nesneyi oluştur
-        //    Instantiate(objectPrefab, clickPosition, Quaternion.identity);
-        //    Debug.Log(Quaternion.identity);
-        //}
         if (Input.GetMouseButtonDown(0))
         {
+            // Mouse pozisyonundan bir ışın oluştur
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            // Işının collider alanıyla temas ettiğini kontrol et
+            if (Physics.Raycast(ray, out hit) && hit.collider == areaCollider)
             {
-                Vector3 clickPosition = hit.point;
-                Instantiate(objectPrefab, clickPosition, Quaternion.identity);
+                Instantiate(objectPrefab, hit.point, Quaternion.identity);
             }
         }
     }
