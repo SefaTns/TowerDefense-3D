8eddda40f555a4f09f9c7835e53d5d4fa8f331e5
SefaTns <sefatns66@gmail.com>
2024-05-25 23:08:20 +0300

    karakterAnim

--- Assets/Scripts/CharacterAnimator/BarbarAnimScript.cs
+++ Assets/Scripts/CharacterAnimator/BarbarAnimScript.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BarbarAnimScript : MonoBehaviour
 {
-    private Animation anim;
+    private Animator anim;
+    private NavMeshAgent agent;
 
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
     private void Start()
     {
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("walkBool", true);
+        agent.SetDestination(MapManager.instance.tower.position);
+        
     }
 
-    private void Update()
+    private void OnTriggerStay(Collider other)
     {
-        anim.Play();
+        if (other.gameObject.CompareTag("leftDoor") || other.gameObject.CompareTag("rightDoor"))
+        {
+            agent.isStopped = true;
+            //anim.SetBool("walkBool", false);
+            anim.SetBool("attackBool", true);
+        }
+        var doorComp = other.gameObject.GetComponent<DoorTrigger>();
+        if(doorComp.DoorHealt == 0)
+        {
+            agent.isStopped = false;
+            anim.SetBool("walkBool", true) ;
+        }
     }
+
+
 }
