72f24b9af7ec1ec5ca66a8174c30d79e9466aca4
Sude Nihan <109664686+SudeNihanGurbuz@users.noreply.github.com>
2024-05-31 08:45:45 +0300

    panel

--- Assets/Scripts/SeviyeScript.cs
+++ Assets/Scripts/SeviyeScript.cs
@@ -9,4 +9,45 @@ public class SeviyeScript : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+    public void Seviye1()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Seviye2()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Seviye3()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Seviye4()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void Seviye5()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void Seviye6()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void Seviye7()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void Seviye8()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void Seviye9()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void Seviye10()
+    {
+        SceneManager.LoadScene(13);
+    }
+
 }
